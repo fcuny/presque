b1ac3c50b3e3dcb36609deea7bf5df33c6a972c9
franck cuny <franck@lumberjaph.net>
2010-04-14 10:38:26 +0200

    basic worker using presque::worker

---
+++ eg/worker.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+use 5.012;
+
+package simple::worker;
+use Moose;
+extends 'presque::worker';
+
+sub work {
+    my ($self, $job) = @_;
+    say "job's done";
+    return;
+}
+
+package main;
+use AnyEvent;
+
+my $worker = simple::worker->new(base_uri => 'http://localhost:5002', queue => 'baz2');
+
+AnyEvent->condvar->recv;
