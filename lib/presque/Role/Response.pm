9e851fb0ed7d9c68e5c447ec25d9a505dce9321a
franck cuny <franck@lumberjaph.net>
2010-06-23 21:05:09 +0200

    remove before for http methods, set content type before finish

--- lib/presque/Role/Response.pm
+++ lib/presque/Role/Response.pm
@@ -2,26 +2,9 @@ package presque::Role::Response;
 
 use Moose::Role;
 
-before get => sub {
-    (shift)->_set_response_content_type;
+before finish => sub {
+    (shift)->response->header('Content-Type' => 'application/json');
 };
-
-before put => sub {
-    (shift)->_set_response_content_type;
-};
-
-before post => sub {
-    (shift)->_set_response_content_type;
-};
-
-before delete => sub {
-    (shift)->_set_response_content_type;
-};
-
-sub _set_response_content_type {
-    my $self = shift;
-    $self->response->header('Content-Type' => 'application/json');
-}
 
 sub entity {
     my ($self, $content) = @_;
